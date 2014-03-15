1b93f1966ad32bf019468eeecf034e64ed429237
Chris Collins <chrishcollins@fullsail.edu>
2014-03-15 17:32:17 -0400

    Add Images

--- Collins_Chris_Project3/Resources/app.js
+++ Collins_Chris_Project3/Resources/app.js
@@ -1,18 +1,19 @@
 var pWidth = Ti.Platform.displayCaps.platformWidth;
 var pHeight = Ti.Platform.displayCaps.platformHeight;
-var itemCount = 30;
+var imageFiles = ["Ambassador.jpg", "AndyMineo.jpg", "DaTRUTH.jpg", "DerekMinor.jpg", "Flame.jpg", "KB.jpg", "Kdrama.jpg", "Lecrae.jpg", "Manafest.jpg", "Propaganda.jpg", "sho-baraka.jpg", "tedashii.jpg", "TripLee.jpg"];
+//var itemCount = 30;  Do not need.  Will us length of array.
 var rowCount = 4;
 var margin = 10;
 var trueCanvasWidth = (pWidth-(margin * (rowCount + 1))); //width of device minus all of the spaces
 var imageSize = (trueCanvasWidth / rowCount); //size determines the size of all of the elements so that they fit in the row and have space between them.
 
 var mainWin = Ti.UI.createWindow({
-	backgroundColor: "#fff",
+	backgroundImage: "Images/dark-fabric.png",
 	layout: "horizontal"	
 });
 
 var border = Ti.UI.createView({
-	backgroundColor: "#cecece",
+	backgroundColor: "transparent",
 	height: 1,
 	width: pWidth,
 	top: 20
@@ -25,10 +26,10 @@ var viewContainer = Ti.UI.createScrollView({
 	contentWidth: pWidth,  //scrollable area
 	height: pHeight - border.height - border.top,
 	showVerticalScrollIndicator: true, //Boolean 
-	backgroundColor: "#fef",
+	backgroundColor: "transparent",
 });
 
-for(var i=0; i<itemCount; i++){
+for(var i=0; i<imageFiles; i++){
 	var view = Ti.UI.createView({
 		backgroundColor: "#33CCFF",
 		borderColor: "transparent",
@@ -39,6 +40,10 @@ for(var i=0; i<itemCount; i++){
 		width: imageSize,
 		height: imageSize	
 	});
+	var newImage = Ti.UI.createImageView({
+		image: "images/" + imageFiles[i]
+	});
+	view.add(newImage);
 	var text = Ti.UI.createLabel({text: i+1, color: "#fff"});
 	view.add(text);
 	viewContainer.add(view);
