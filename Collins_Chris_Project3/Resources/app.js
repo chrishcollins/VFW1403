4196bd62edb24d8906ab8a0558f53ba2a8c7ec85
Chris Collins <chrishcollins@fullsail.edu>
2014-03-15 16:23:50 -0400

    Initial Commit

--- Collins_Chris_Project3/Resources/app.js
+++ Collins_Chris_Project3/Resources/app.js
@@ -1,64 +1,49 @@
-// this sets the background color of the master UIView (when there are no windows/tab groups on it)
-Titanium.UI.setBackgroundColor('#000');
+var pWidth = Ti.Platform.displayCaps.platformWidth;
+var pHeight = Ti.Platform.displayCaps.platformHeight;
+var itemCount = 30;
+var rowCount = 4;
+var margin = 10;
+var trueCanvasWidth = (pWidth-(margin * (rowCount + 1))); //width of device minus all of the spaces
+var imageSize = (trueCanvasWidth / rowCount); //size determines the size of all of the elements so that they fit in the row and have space between them.
 
-// create tab group
-var tabGroup = Titanium.UI.createTabGroup();
-
-
-//
-// create base UI tab and root window
-//
-var win1 = Titanium.UI.createWindow({  
-    title:'Tab 1',
-    backgroundColor:'#fff'
-});
-var tab1 = Titanium.UI.createTab({  
-    icon:'KS_nav_views.png',
-    title:'Tab 1',
-    window:win1
+var mainWin = Ti.UI.createWindow({
+	backgroundColor: "#fff",
+	layout: "horizontal"	
 });
 
-var label1 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 1',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
+var border = Ti.UI.createView({
+	backgroundColor: "#cecece",
+	height: 1,
+	width: pWidth,
+	top: 20
 });
 
-win1.add(label1);
-
-//
-// create controls tab and root window
-//
-var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
-    backgroundColor:'#fff'
-});
-var tab2 = Titanium.UI.createTab({  
-    icon:'KS_nav_ui.png',
-    title:'Tab 2',
-    window:win2
+var viewContainer = Ti.UI.createScrollView({
+	top: 0,
+	layout: "horizontal",
+	width: pWidth,
+	contentWidth: pWidth,  //scrollable area
+	height: pHeight - border.height - border.top,
+	showVerticalScrollIndicator: true, //Boolean 
+	backgroundColor: "#fef",
 });
 
-var label2 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 2',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
+for(var i=0; i<itemCount; i++){
+	var view = Ti.UI.createView({
+		backgroundColor: "#33CCFF",
+		borderColor: "transparent",
+		borderSize: 1,
+		borderRadius: 5,
+		top: margin,
+		left: margin,
+		width: imageSize,
+		height: imageSize	
+	});
+	var text = Ti.UI.createLabel({text: i+1, color: "#fff"});
+	view.add(text);
+	viewContainer.add(view);
+}
 
-win2.add(label2);
-
-
-
-//
-//  add tabs
-//
-tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
-
-
-// open tab group
-tabGroup.open();
+mainWin.add(border);
+mainWin.add(viewContainer);
+mainWin.open();
